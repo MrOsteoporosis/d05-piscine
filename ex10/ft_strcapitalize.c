3b30a882799a6c8cdf592d024926c9d7b5ea107c
Andreas Verheijen <averheij@student.codam.nl>
2019-09-16 13:30:31 +0200

    exercise 10

---
+++ ex10/ft_strcapitalize.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strcapitalize.c                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/16 12:42:33 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/16 13:30:16 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_strlen(char *str)
+{
+	int length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length = length + 1;
+	}
+	return (length);
+}
+
+int		ft_islpha(char c)
+{
+	char	alphabet[52];
+	int		temp;
+
+	alphabet[0] = 'A';
+	alphabet[26] = 'a';
+	temp = 1;
+	while (temp < 26)
+	{
+		alphabet[temp] = alphabet[temp - 1] + 1;
+		alphabet[temp + 26] = alphabet[temp - 1 + 26] + 1;
+		temp++;
+	}
+	temp = 0;
+	while (temp < 52)
+	{
+		if (c == alphabet[temp])
+		{
+			return (1);
+		}
+		temp++;
+	}
+	return (0);
+}
+
+char	*ft_strlowcase(char *str)
+{
+	int difference;
+	int length;
+	int temp;
+
+	difference = 0;
+	length = ft_strlen(str);
+	temp = 0;
+	while (temp < length)
+	{
+		if (str[temp] > 64 && str[temp] < 91)
+		{
+			str[temp] += 32;
+		}
+		temp++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int difference;
+	int length;
+	int t;
+
+	difference = 0;
+	length = ft_strlen(str);
+	ft_strlowcase(str);
+	if (ft_islpha(str[0]) && str[0] > 96 && str[0] < 123)
+	{
+		str[0] -= 32;
+	}
+	t = 1;
+	while (t < length)
+	{
+		if ((!ft_islpha(str[t - 1])) && str[t] > 96 && str[t] < 123)
+		{
+			str[t] -= 32;
+		}
+		t++;
+	}
+	return (str);
+}
---
+++ ex10/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/16 12:41:25 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/16 13:17:29 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcapitalize(char *str);
+
+int		main(void)
+{
+	char str1[] = "salut, comment tu vas ? 42mots quassrante-deux; cinquante+et+un";
+
+	printf("%s\n", str1);
+	printf("%s", ft_strcapitalize(str1));
+}
