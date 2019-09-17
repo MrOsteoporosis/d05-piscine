fb0a7d740b1119916771824b60e30fcba5b2172b
Andreas Verheijen <averheij@student.codam.nl>
2019-09-17 13:49:31 +0200

    ex02 recusive

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,37 +6,26 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/16 09:22:22 by averheij       #+#    #+#                */
-/*   Updated: 2019/09/16 09:26:01 by averheij      ########   odam.nl         */
+/*   Updated: 2019/09/17 13:49:10 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_putnbr(int nb)
+void	ft_recurputnbr(int nb)
 {
-	int temp;
-	int factor;
-
-	factor = 1;
 	if (nb < 0)
 	{
-		nb = nb * -1;
 		ft_putchar('-');
+		nb = -nb;
 	}
-	temp = nb;
-	while (temp)
-	{
-		temp = temp / 10;
-		factor = factor * 10;
-	}
-	if (nb == 0)
-	{
-		ft_putchar('0');
-	}
-	while (factor > 1)
-	{
-		factor = factor / 10;
-		ft_putchar('0' + (nb / factor) - ((nb / (factor * 10)) * 10));
-	}
+	if (nb / 10)
+		ft_recurputnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	ft_recurputnbr(nb);
 	ft_putchar('\n');
 }
